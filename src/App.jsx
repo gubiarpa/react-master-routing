3287671f74e861e426ee8ce4ce40450ab4feac1d
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-09-15 20:43:53 -0500

    Temp removing lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { lazy, Suspense } from 'react'
+// import { lazy, Suspense } from 'react'
 
 import { Router } from './Router'
 import { Route } from './Route'
 
 import HomePage from './pages/Home.jsx'
+import AboutPage from './pages/About.jsx'
+import SearchPage from './pages/Search.jsx'
+import NotFoundPage from './pages/404.jsx'
 
 import './App.css'
 
-const AboutPage = lazy(() => import('./pages/About.jsx'))
-const NotFoundPage = lazy(() => import('./pages/404.jsx'))
-const SearchPage = lazy(() => import('./pages/Search.jsx'))
+// const AboutPage = lazy(() => import('./pages/About.jsx'))
+// const NotFoundPage = lazy(() => import('./pages/404.jsx'))
+// const SearchPage = lazy(() => import('./pages/Search.jsx'))
+{
+	/* <Suspense fallback={<div>Loading...</div>}>
+</Suspense> */
+}
 
 const appRoutes = [{ path: '/search/:query', Component: SearchPage }]
 
@@ -17,21 +24,19 @@ function App() {
 	return (
 		<>
 			<main>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Router
-						routes={appRoutes}
-						defaultComponent={NotFoundPage}
-					>
-						<Route
-							path={'/'}
-							Component={HomePage}
-						/>
-						<Route
-							path={'/about'}
-							Component={AboutPage}
-						/>
-					</Router>
-				</Suspense>
+				<Router
+					routes={appRoutes}
+					defaultComponent={NotFoundPage}
+				>
+					<Route
+						path={'/'}
+						Component={HomePage}
+					/>
+					<Route
+						path={'/about'}
+						Component={AboutPage}
+					/>
+				</Router>
 			</main>
 		</>
 	)
